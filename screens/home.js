de4f22304c83f4ede5e93e56de5feb6e0644244a
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-29 19:29:10 +0530

    form added with formik

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import {StyleSheet,View,Text, Button} from 'react-native'
+import {StyleSheet,View,Text, Button,Modal} from 'react-native'
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
+import { MaterialIcons } from '@expo/vector-icons';
+import ReviewForm from './reviewfoorms';
 
 export default function Home({navigation}){
 
@@ -10,7 +12,7 @@ export default function Home({navigation}){
         navigation.navigate('ReviewDetails')
         //navigation.pop('ReviewDetails')
     }
-
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews,setReviews]=useState([
         {title:'abc',rating:5,body:'lorem ipsum',key:'1'},
         {title:'pqr',rating:4,body:'lorem ipsum',key:'2'},
@@ -24,6 +26,23 @@ export default function Home({navigation}){
             </Text> */}
             {/* <Button title='go to dets'
             onPress={pressHandler}/> */}
+            <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons 
+            name='close'
+            size={24} 
+            style={{...styles.modalToggle, ...styles.modalClose}} 
+            onPress={() => setModalOpen(false)} 
+          />
+          <ReviewForm/>
+        </View>
+      </Modal>
+      <MaterialIcons 
+        name='add' 
+        size={24} 
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)} 
+      />
             <FlatList
             data={reviews}
             renderItem={({item})=>(
@@ -38,6 +57,28 @@ export default function Home({navigation}){
     )
 
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 10,
+      borderWidth: 1,
+      borderColor: '#f2f2f2',
+      padding: 10,
+      borderRadius: 10,
+      alignSelf: 'center',
+    },
+    modalClose: {
+      marginTop: 20,
+      marginBottom: 0,
+    },
+    modalContent: {
+      flex: 1,
+      //alignItems: 'center',
+      
+    }
+  });
 
 
   

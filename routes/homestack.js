9198dd1eb01017fef5ce719f714ce48218d35077
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-29 13:28:51 +0530

    header customized

--- routes/homestack.js
+++ routes/homestack.js
@@ -6,16 +6,31 @@ import About from '../screens/about'
 
 const screens = {
     Home:{
-        screen:Home
+        screen:Home,
+        navigationOptions:{
+            title:'Game Zone',
+            //headerStyle:{backgroundColor:'#eee'}
+        }
     },
     ReviewDetails:{
-        screen:ReviewDetails
+        screen:ReviewDetails,
+        navigationOptions:{
+            title:'Review Details',
+            //headerStyle:{backgroundColor:'#eee'}
+
+        }
     },
     About:{
         screen:About
     }
 }
 
-const HomeStack = createStackNavigator(screens)
+const HomeStack = createStackNavigator(screens,{
+    defaultNavigationOptions:{
+        headerTintColor:'#444',
+        headerStyle:{backgroundColor:'#eee',
+    height:60}
+    }
+})
 
 export default createAppContainer(HomeStack)
